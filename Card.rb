aeac8b35a9a92919fde5ea430743b8d3851ffd33
Alexander Kotov <amikotov@gmail.com>
2016-08-26 08:15:58 +0300

    Initial commit

---
+++ Card.rb
@@ -0,0 +1,11 @@
+class Card
+  attr_accessor :suit # Suit of card
+  attr_accessor :value # 1, 2, 3, 4,.., K, Ace
+  attr_accessor :open # Position of card on the table: open or not-open (default = true)
+  attr_accessor :points # Points for this :value of card
+  attr_accessor :used # Already used on this game or not ( default = false)
+
+
+def initialize
+  
+end
