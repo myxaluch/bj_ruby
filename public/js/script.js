c5b1581a3768fabf591c5488ae5f4c7faf74cd24
Alexander Kotov <amikotov@gmail.com>
2016-08-29 17:15:18 +0300

    [WIP] Delete excess .js files

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#hit").click(function(){
-    $("#p").load("/play",function(responseTxt,statusTxt,xhr){
-      if(statusTxt=="error")
-        alert("Error: "+xhr.status+": "+xhr.statusText);
-    });
-  });
-});
